f1bd2706b81ffff8e46998d5d475fee805208ef2
unknown <suneet@apptunix.com>
2021-09-21 12:54:57 +0530

    21-09-21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:'http://182.72.246.251:1402/api/auth/admin/',
-  storageKey: 'beaconBoxAdminLogin',
+  baseUrl:'http://182.72.246.251:1402/api/',
+  storageKey: 'token',
 };
