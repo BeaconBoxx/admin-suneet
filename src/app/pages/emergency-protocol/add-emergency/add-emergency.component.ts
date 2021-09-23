0044f520822053c8b2e1b11645c65274c873ae3d
unknown <suneet@apptunix.com>
2021-09-23 18:37:25 +0530

    new changes

--- src/app/pages/emergency-protocol/add-emergency/add-emergency.component.ts
+++ src/app/pages/emergency-protocol/add-emergency/add-emergency.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from '../../../_services/common.service';
+import { urls } from '../../../_services/urls';
 
 @Component({
   selector: 'app-add-emergency',
   templateUrl: './add-emergency.component.html',
-  styleUrls: ['./add-emergency.component.css']
+  styleUrls: ['./add-emergency.component.scss']
 })
 export class AddEmergencyComponent implements OnInit {
-
-  constructor() { }
+  userId: any;
+  items: any;
+  constructor(private route: ActivatedRoute,private commn_: CommonService) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.userId= params.id;
+      this.getUserById();
+    });
+  }
+  
+  
+  getUserById()
+  {
+    this.commn_.get(urls.getUserDetailsById+this.userId+"/").subscribe(res=>{
+      console.log(res);
+      this.items=res?.data;
+    });
+  }
+  
+  back()
+  {
+    history.back();
   }
 
 }
